34eaa8793ac5c1aab5df41957b30fd8a56bece90
dykim <doyune0707@gmail.com>
2023-01-18 10:34:19 +0900

    [feat] 프로젝트 생성

---
+++ PokeDex/PokeDex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by 김도윤 on 2023/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
