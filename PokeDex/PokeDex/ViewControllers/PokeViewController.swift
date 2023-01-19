8460eb8a4cffd821c779228afd5242b5149c8ba6
dykim <doyune0707@gmail.com>
2023-01-19 14:34:37 +0900

    [feat] 포켓몬 리스트 1차 구현

--- PokeDex/PokeDex/ViewControllers/PokeViewController.swift
+++ PokeDex/PokeDex/ViewControllers/PokeViewController.swift
@@ -6,16 +6,48 @@
 //
 
 import UIKit
+import Kingfisher
 
 class PokeViewController: UIViewController {
 
-    var poke = [Pokemon]()
+    @IBOutlet weak var collectionView: UICollectionView!
+    private let apiService = APIService.shared
+    private var pokemons = [Pokemon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        APIService().fetchPokemon(startIndex: 0, endIndex: 30) { result in
-            self.poke = result
+        collectionViewSetup()
+        apiService.fetchPokemon(startIndex: 0, endIndex: 30) { result in
+            self.pokemons = result
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
         }
     }
+    
+    private func collectionViewSetup() {
+        collectionView.dataSource = self
+        collectionView.collectionViewLayout = TwoColumnFlowLayout()
+    }
+}
+
+//MARK: CollectionView DataSource
+extension PokeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pokemons.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? PokemonCollectionViewCell {
+            let list = pokemons[indexPath.row]
+            cell.nameLabel.text = list.name
+            cell.backView.backgroundColor = cell.getColor(type: list.type ?? "")
+            cell.imageView.kf.setImage(with: URL(string: list.imageUrl ?? ""))
+            return cell
+        }
+        
+        return UICollectionViewCell()
+    }
+    
     
 }
