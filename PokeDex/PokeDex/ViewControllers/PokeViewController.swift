bfaf86592fad22b89043e83481963530febb1739
dykim <doyune0707@gmail.com>
2023-01-19 10:33:01 +0900

    [feat] API 테스트

--- PokeDex/PokeDex/ViewControllers/PokeViewController.swift
+++ PokeDex/PokeDex/ViewControllers/PokeViewController.swift
@@ -9,21 +9,13 @@ import UIKit
 
 class PokeViewController: UIViewController {
 
+    var poke = [Pokemon]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        APIService().fetchPokemon(startIndex: 0, endIndex: 30) { result in
+            self.poke = result
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
