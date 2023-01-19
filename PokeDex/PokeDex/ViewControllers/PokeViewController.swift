bf5f10cf65c8d547797d9f878e6f23ac6162631a
dykim <doyune0707@gmail.com>
2023-01-19 17:59:24 +0900

    [feat] 당겨서 새로고침 추가

--- PokeDex/PokeDex/ViewControllers/PokeViewController.swift
+++ PokeDex/PokeDex/ViewControllers/PokeViewController.swift
@@ -14,20 +14,43 @@ class PokeViewController: UIViewController {
     private let apiService = APIService.shared
     private var pokemons = [Pokemon]()
     
+    private var startIndex = 1
+    private var endIndex = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionViewSetup()
-        apiService.fetchPokemon(startIndex: 0, endIndex: 30) { result in
+        fetchPokemon(startIndex: startIndex, endIndex: endIndex)
+    }
+    
+    //MARK: 리스트 스크롤 새로고침
+    private func initListViewRefresh() {
+        let refresh = UIRefreshControl()
+        refresh.tintColor = .black
+        refresh.addTarget(self, action: #selector(listRefresh(refresh: )), for: .valueChanged)
+        collectionView.refreshControl = refresh
+    }
+    
+
+    //MARK: 새로고침이 끝났을때
+    @objc func listRefresh(refresh: UIRefreshControl) {
+        fetchPokemon(startIndex: startIndex, endIndex: endIndex)
+        refresh.endRefreshing()
+    }
+    
+    private func fetchPokemon(startIndex: Int, endIndex: Int) {
+        apiService.fetchPokemon(startIndex: startIndex, endIndex: endIndex) { result in
             self.pokemons = result
             DispatchQueue.main.async {
                 self.collectionView.reloadData()
             }
         }
     }
-    
+
     private func collectionViewSetup() {
         collectionView.dataSource = self
         collectionView.collectionViewLayout = TwoColumnFlowLayout()
+        initListViewRefresh()
     }
 }
 
