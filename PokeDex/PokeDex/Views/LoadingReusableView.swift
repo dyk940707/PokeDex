95b0b1d8f5e64f1c2f9b45e046e3346c4635fa25
dykim <doyune0707@gmail.com>
2023-01-20 11:59:21 +0900

    [feat] 로딩 Footer 생성

---
+++ PokeDex/PokeDex/Views/LoadingReusableView.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingReusableView.swift
+//  PokeDex
+//
+//  Created by 김도윤 on 2023/01/19.
+//
+
+import UIKit
+
+class LoadingReusableView: UICollectionReusableView {
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
