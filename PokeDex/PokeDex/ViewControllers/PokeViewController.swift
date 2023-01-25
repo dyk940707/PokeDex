415e3880ccd523d4a062fdf469e579303e3ddfa5
dykim <doyune0707@gmail.com>
2023-01-25 17:45:01 +0900

    [feat] 로딩 추가 및 버그 발견..

--- PokeDex/PokeDex/ViewControllers/PokeViewController.swift
+++ PokeDex/PokeDex/ViewControllers/PokeViewController.swift
@@ -16,9 +16,13 @@ class PokeViewController: UIViewController {
     
     private var startIndex = 1
     private var endIndex = 50
-    
+    var isLoading = false
+    var loadingView: LoadingReusableView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        let loadingReusableNib = UINib(nibName: "LoadingReusableView", bundle: nil)
+        collectionView.register(loadingReusableNib, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "loadingresuableviewid")
         collectionViewSetup()
         fetchPokemon(startIndex: startIndex, endIndex: endIndex)
     }
@@ -49,17 +53,31 @@ class PokeViewController: UIViewController {
 
     private func collectionViewSetup() {
         collectionView.dataSource = self
+        collectionView.delegate = self
         collectionView.collectionViewLayout = TwoColumnFlowLayout()
         initListViewRefresh()
     }
 }
 
 //MARK: CollectionView DataSource
-extension PokeViewController: UICollectionViewDataSource {
+extension PokeViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        if self.isLoading {
+            return CGSize.zero
+        } else {
+            return CGSize(width: collectionView.bounds.size.width, height: 55)
+        }
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return pokemons.count
     }
     
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.row == pokemons.count-1, !self.isLoading {
+            loadMoreData()
+        }
+    }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? PokemonCollectionViewCell {
             let list = pokemons[indexPath.row]
@@ -71,6 +89,38 @@ extension PokeViewController: UICollectionViewDataSource {
         
         return UICollectionViewCell()
     }
-    
-    
+    func loadMoreData() {
+        if !self.isLoading {
+            self.isLoading = true
+            DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) {
+                DispatchQueue.main.async {
+                    print("load more")
+                    self.fetchPokemon(startIndex: self.startIndex, endIndex: self.endIndex+50)
+                    self.collectionView.reloadData()
+                    self.isLoading = false
+                }
+            }
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionFooter {
+            let aFooterView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "loadingresuableviewid", for: indexPath) as! LoadingReusableView
+            loadingView = aFooterView
+            loadingView?.backgroundColor = UIColor.clear
+            return aFooterView
+        }
+        return UICollectionReusableView()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, willDisplaySupplementaryView view: UICollectionReusableView, forElementKind elementKind: String, at indexPath: IndexPath) {
+        if elementKind == UICollectionView.elementKindSectionFooter {
+            self.loadingView?.activityIndicator.startAnimating()
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didEndDisplayingSupplementaryView view: UICollectionReusableView, forElementOfKind elementKind: String, at indexPath: IndexPath) {
+        if elementKind == UICollectionView.elementKindSectionFooter {
+            self.loadingView?.activityIndicator.stopAnimating()
+        }
+    }
 }
